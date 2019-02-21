7f63c2d0d3af698e378d4814b7e95ed7de2804d3
RaulParraM <raosprat@hotmail.com>
2019-02-21 12:55:30 +0100

    second commit

---
+++ MulticastDelegate/MiClase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    class MiClase
+    {
+        public void MiMetodo1(int i)
+        {
+            Console.WriteLine("El valor pasado al METODO1 como parámetro es: {0}", i);
+        }
+        public void MiMetodo2(int i)
+        {
+            Console.WriteLine("El valor pasado al METODO2 como parámetro es: {0}", i);
+        }
+    }
+}
---
+++ MulticastDelegate/Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    public delegate void MiDelegadoMultiCast(int i);
+    class Test
+    {
+        public static void Main()
+        {
+            MiClase miClase = new MiClase();
+
+            // instanciamos un delegado. Observa que el método MiMetodo1 de la clase 
+            // MiClase cumple con la firma definida en el delegado
+            MiDelegadoMultiCast delegado = miClase.MiMetodo1;
+            delegado(83); // Se hace la llamada al delegado
+
+            // agregamos ahora el Metodo2 
+            delegado += miClase.MiMetodo2;
+            // Se hace la llamada al delegado y se ejecutarán en orden secuencial ambos métodos
+            delegado(55);
+
+            // eliminamos ahora el Metodo1 
+            delegado -= miClase.MiMetodo1;
+            // Se hace la llamada al delegado y se ejecutará únicamente el Método2
+            delegado(14);
+ 
+        }
+    }
+}
